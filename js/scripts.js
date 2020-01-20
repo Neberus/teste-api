c5e64b2c1438ad5bd7273b1cf5aa48b0ff9cc55b
unknown <Digital@DH-A10-17.dhbrasil.digitalhouse.com>
2020-01-20 11:50:18 -0300

    Integrar um sistema de API

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,32 @@
 let buttonClick = document.getElementById('btn')
+let board = document.getElementById('board')
 
 buttonClick.addEventListener('click', ()=>{
     fetch("https://newsapi.org/v2/top-headlines?country=br&apiKey=e6a861b23a474a97af3e647718c37557"). then((response)=>{
         return response.json();
 
     }).then(json=>{
-        console.log(json)
+        console.log(json.articles)
+        mostrarNaTela(json.articles);
     })
 })
+
+let mostrarNaTela = listaNoticias =>{
+// for(let noticia of listaNoticias)
+
+    listaNoticias.forEach(function(noticia, posicao){
+
+        let card = ` <div class="card text-center" style="width: 18rem;">
+        <img class="card-img-top" src="${noticia.urlToImage}" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">${noticia.title}</h5>
+            <p class="card-text">${noticia.description}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>`
+        board.innerHTML += card;
+    })
+}
 
 // ()=>"solução direta sem return";
 // ()+>{
